b15f820510c57692baf81980ff4289a17e30dd27
Grégory Clain <g.clain69@gmail.com>
2022-11-20 00:54:10 +0100

    Add default page

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,14 +3,13 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class BlogController
+class BlogController extends AbstractController
 {
     #[Route('/')]
     public function blogList(): Response
     {
-        return new Response(
-            '<html lang="fr"><body>Test affichage</body></html>'
-        );
+        return $this->render('blogList.html.twig',[]);
     }
 }
